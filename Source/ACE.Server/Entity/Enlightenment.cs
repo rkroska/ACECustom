7cf27e09c969f9410d8c4034ef61bba0a6b0f480
Rudy Kroska <rkroska@users.noreply.github.com>
2023-12-01 19:53:26 -0600

    enlighten to discord

--- Source/ACE.Server/Entity/Enlightenment.cs
+++ Source/ACE.Server/Entity/Enlightenment.cs
@@ -505,6 +505,7 @@ namespace ACE.Server.Entity
 
             var msg = $"{player.Name} has achieved the {lvl} level of Enlightenment!";
             PlayerManager.BroadcastToAll(new GameMessageSystemChat(msg, ChatMessageType.WorldBroadcast));
+            DiscordChatManager.SendDiscordMessage(player.Name, msg, ConfigManager.Config.Chat.GeneralChannelId);
             PlayerManager.LogBroadcastChat(Channel.AllBroadcast, null, msg);
 
             // +2 vitality
