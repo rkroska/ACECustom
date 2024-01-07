c383512f30777f42fe79d28bdb66557ba229e339
Rudy Kroska <rkroska@users.noreply.github.com>
2024-01-07 14:15:01 -0600

    new toon bank fix

--- Source/ACE.Server/Command/Handlers/PlayerCommands.cs
+++ Source/ACE.Server/Command/Handlers/PlayerCommands.cs
@@ -320,7 +320,7 @@ namespace ACE.Server.Command.Handlers
                 {
                     case 1:
                         //withdraw pyreals
-                        if (amount > session.Player.BankedPyreals)
+                        if (session.Player.BankedPyreals != null && amount >= session.Player.BankedPyreals)
                         {
                             session.Network.EnqueueSend(new GameMessageSystemChat($"You do not have enough pyreals banked to make this withdrawl", ChatMessageType.System));
                             break;
@@ -334,7 +334,7 @@ namespace ACE.Server.Command.Handlers
                         break;
                     case 2:
                         //withdraw lum
-                        if (amount > session.Player.BankedLuminance)
+                        if (session.Player.BankedLuminance != null && amount >= session.Player.BankedLuminance)
                         {
                             session.Network.EnqueueSend(new GameMessageSystemChat($"You do not have enough luminance banked to make this withdrawl", ChatMessageType.System));
                             break;
@@ -352,7 +352,7 @@ namespace ACE.Server.Command.Handlers
                         session.Player.WithdrawLuminance(amount);
                         break;
                     case 4:
-                        if (amount > session.Player.BankedLegendaryKeys)
+                        if (session.Player.BankedLegendaryKeys != null && amount >= session.Player.BankedLegendaryKeys)
                         {
                             session.Network.EnqueueSend(new GameMessageSystemChat($"You do not have enough keys banked to make this withdrawl", ChatMessageType.System));
                             break;
@@ -370,7 +370,7 @@ namespace ACE.Server.Command.Handlers
                         session.Player.WithdrawLegendaryKeys(amount);
                         break;
                     case 6:
-                        if (amount > session.Player.BankedEnlightenedCoins)
+                        if (session.Player.BankedEnlightenedCoins != null && amount >= session.Player.BankedEnlightenedCoins)
                         {
                             session.Network.EnqueueSend(new GameMessageSystemChat($"You do not have enough enlightened coins banked to make this withdrawl", ChatMessageType.System));
                             break;
@@ -404,7 +404,7 @@ namespace ACE.Server.Command.Handlers
                 {
                     case 1:
                         //transfer pyreals
-                        if (amount > session.Player.BankedPyreals)
+                        if (session.Player.BankedPyreals != null && amount >= session.Player.BankedPyreals)
                         {
                             session.Network.EnqueueSend(new GameMessageSystemChat($"You do not have enough pyreals banked to make this transfer", ChatMessageType.System));
                             break;
@@ -426,7 +426,7 @@ namespace ACE.Server.Command.Handlers
                         break;
                     case 2:
                         //transfer lum
-                        if (amount > session.Player.BankedLuminance)
+                        if (session.Player.BankedLuminance != null && amount >= session.Player.BankedLuminance)
                         {
                             session.Network.EnqueueSend(new GameMessageSystemChat($"You do not have enough luminance banked to make this transfer", ChatMessageType.System));
                             break;
@@ -446,7 +446,7 @@ namespace ACE.Server.Command.Handlers
                         }
                         break;
                     case 4:
-                        if (amount > session.Player.BankedLegendaryKeys)
+                        if (session.Player.BankedLegendaryKeys != null && amount >= session.Player.BankedLegendaryKeys)
                         {
                             session.Network.EnqueueSend(new GameMessageSystemChat($"You do not have enough keys banked to make this transfer", ChatMessageType.System));
                             break;
@@ -466,7 +466,7 @@ namespace ACE.Server.Command.Handlers
                         }
                         break;
                     case 6:
-                        if (amount > session.Player.BankedEnlightenedCoins)
+                        if (session.Player.BankedEnlightenedCoins != null && amount >= session.Player.BankedEnlightenedCoins)
                         {
                             session.Network.EnqueueSend(new GameMessageSystemChat($"You do not have enough enlightened coins banked to make this transfer", ChatMessageType.System));
                             break;
