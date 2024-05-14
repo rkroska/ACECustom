45228efb53c0555b50fc9f03df389e6b79c505ab
Rudy Kroska <rkroska@users.noreply.github.com>
2024-05-13 19:51:33 -0500

    sesssion speedup

--- Source/ACE.Server/Network/Managers/NetworkManager.cs
+++ Source/ACE.Server/Network/Managers/NetworkManager.cs
@@ -355,22 +355,20 @@ namespace ACE.Server.Network.Managers
             {
                 // The session tick outbound processes pending actions and handles outgoing messages
                 ServerPerformanceMonitor.RestartEvent(ServerPerformanceMonitor.MonitorType.DoSessionWork_TickOutbound);
-                Parallel.ForEach(sessionMap, ConfigManager.Config.Server.Threading.NetworkManagerParallelOptions, s => s?.TickOutbound());
-                ServerPerformanceMonitor.RegisterEventEnd(ServerPerformanceMonitor.MonitorType.DoSessionWork_TickOutbound);
-
-                // Removes sessions in the NetworkTimeout state, including sessions that have reached a timeout limit.
-                ServerPerformanceMonitor.RestartEvent(ServerPerformanceMonitor.MonitorType.DoSessionWork_RemoveSessions);
-                foreach (var session in sessionMap.Where(k => !Equals(null, k)))
+                Parallel.ForEach(sessionMap, ConfigManager.Config.Server.Threading.NetworkManagerParallelOptions, s =>
                 {
-                    if (session.PendingTermination != null && session.PendingTermination.TerminationStatus == SessionTerminationPhase.SessionWorkCompleted)
+                    if (s != null)
                     {
-                        session.DropSession();
-                        session.PendingTermination.TerminationStatus = SessionTerminationPhase.WorldManagerWorkCompleted;
+                        s.TickOutbound();
+                        if (s.PendingTermination != null && s.PendingTermination.TerminationStatus == SessionTerminationPhase.SessionWorkCompleted)
+                        {
+                            s.DropSession();
+                            s.PendingTermination.TerminationStatus = SessionTerminationPhase.WorldManagerWorkCompleted;
+                        }
+                        sessionCount++;
                     }
-
-                    sessionCount++;
-                }
-                ServerPerformanceMonitor.RegisterEventEnd(ServerPerformanceMonitor.MonitorType.DoSessionWork_RemoveSessions);
+                });
+                ServerPerformanceMonitor.RegisterEventEnd(ServerPerformanceMonitor.MonitorType.DoSessionWork_TickOutbound);
             }
             finally
             {
@@ -378,6 +376,7 @@ namespace ACE.Server.Network.Managers
             }
             return sessionCount;
         }
+
 
         public static void DisconnectAllSessionsForShutdown()
         {
