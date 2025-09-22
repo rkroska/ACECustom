c4f7888334ebda32ff2acbd15963d05b9688ac6d
Schneebs <27392240+Schneebs@users.noreply.github.com>
2025-09-21 21:47:51 -0400

    added caching for duration until recheck (#195)
    * added caching for duration until recheck
    
    * coderabbit updates
    
    * fixed a bug with monsters not aggroing and some other tweaks
    
    * rabbit suggetions
    
    * wabbit found inconsistencies
    
    * more coderabbit suggestions
    
    * yet another... coderabbit suggestion
    
    * more coderabbit changes
    
    * add system threading and null checks
    
    * more coderabbit findings

--- Source/ACE.Server/WorldObjects/Monster_Tick.cs
+++ Source/ACE.Server/WorldObjects/Monster_Tick.cs
@@ -7,7 +7,7 @@ namespace ACE.Server.WorldObjects
 {
     partial class Creature
     {
-        protected const double monsterTickInterval = 0.2;
+        protected const double monsterTickInterval = 0.3;
 
         public double NextMonsterTickTime;
 
