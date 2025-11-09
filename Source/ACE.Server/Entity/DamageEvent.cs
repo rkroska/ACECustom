35967b950294f7e92176d9f9689e843661959464
Rudy Kroska <rkroska@users.noreply.github.com>
2025-11-09 10:38:55 -0600

    Revert "I Summon thee... (#228)"
    This reverts commit 405ba1a8f094964761d7f1bf197def11b421dd18.

--- Source/ACE.Server/Entity/DamageEvent.cs
+++ Source/ACE.Server/Entity/DamageEvent.cs
@@ -241,19 +241,7 @@ namespace ACE.Server.Entity
                 damageBonus = cachedLuminanceAugmentCount.Value;
             }
 
-            // Add melee/missile aug bonus to BaseDamageMod.DamageBonus so it affects both min and max damage
-            // This ensures both item augs and melee/missile augs properly affect the damage range
-            if (BaseDamageMod != null && damageBonus > 0)
-            {
-                BaseDamageMod.DamageBonus += damageBonus;
-                // Re-roll BaseDamage now that DamageBonus has been updated (affects both min and max)
-                BaseDamage = (float)ThreadSafeRandom.Next(BaseDamageMod.MinDamage, BaseDamageMod.MaxDamage);
-            }
-            else if (damageBonus > 0)
-            {
-                // Fallback: if BaseDamageMod is null, add directly to BaseDamage
-                BaseDamage += damageBonus;
-            }
+            BaseDamage += damageBonus;
 
             // get damage modifiers
             PowerMod = attacker.GetPowerMod(Weapon);
@@ -346,14 +334,7 @@ namespace ACE.Server.Entity
             // armor rending and cleaving
             var armorRendingMod = 1.0f;
             if (Weapon != null && Weapon.HasImbuedEffect(ImbuedEffectType.ArmorRending))
-            {
                 armorRendingMod = WorldObject.GetArmorRendingMod(attackSkill);
-            }
-            else if (attacker is CombatPet && attacker.HasImbuedEffect(ImbuedEffectType.ArmorRending))
-            {
-                // For CombatPets without weapons, check if armor rending was applied to the creature itself
-                armorRendingMod = WorldObject.GetArmorRendingMod(attackSkill);
-            }
 
             var armorCleavingMod = attacker.GetArmorCleavingMod(Weapon);
 
