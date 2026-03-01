a1b948d432dd3a4ba5ed115cfc77728e8a161b57
MastaMageOg <kdonah3@gmail.com>
2026-02-28 19:15:17 -0500

    feat: add /shinies player command to list shiny pet collection (#368)

--- Source/ACE.Server/Entity/PetRegistryManager.cs
+++ Source/ACE.Server/Entity/PetRegistryManager.cs
@@ -295,6 +295,24 @@ namespace ACE.Server.Entity
             }
         }
 
+
+        /// <summary>
+        /// Get only shiny pets for a specific account, ordered by registration date.
+        /// </summary>
+        /// <param name="accountId">The account ID to retrieve shiny pets for.</param>
+        /// <returns>A list of tuples containing WCID, creature name, and registration timestamp.</returns>
+        public static List<(uint Wcid, string Name, DateTime RegisteredAt)> GetShinyRegistry(uint accountId)
+        {
+            using (var context = new ShardDbContext())
+            {
+                return context.PetRegistry
+                    .Where(p => p.AccountId == accountId && p.IsShiny)
+                    .OrderBy(p => p.RegisteredAt)
+                    .Select(p => new ValueTuple<uint, string, DateTime>(p.Wcid, p.CreatureName, p.RegisteredAt))
+                    .ToList();
+            }
+        }
+
         public static List<PetRegistryEntry> GetFullPetRegistry(uint accountId)
         {
              using (var context = new ShardDbContext())
