ab3b474b252558522b858a16c143f2ea5226ee9f
Ruggan <141803970+Ruggan@users.noreply.github.com>
2026-04-03 08:58:44 -0500

    Move some disciplinary commands to a new file.
    Also update the .gitignore file to prevent accidental inclusion of front end files.

--- Source/ACE.Server/Command/Handlers/AdminCommands.cs
+++ Source/ACE.Server/Command/Handlers/AdminCommands.cs
@@ -49,34 +49,7 @@ namespace ACE.Server.Command.Handlers
         //     //TODO: output
         // }
 
-        [CommandHandler("jail", AccessLevel.Admin, CommandHandlerFlag.RequiresWorld,
-            "Sends a player to jail.",
-            "Usage: /jail [playername]\nIf no name is provided, the currently selected player is used.")]
-        public static void HandleJail(Session session, params string[] parameters)
-        {
-            Player target = CommandHandlerHelper.GetPlayerAsCommandTarget(session, string.Join(" ", parameters));
-            if (target == null) return;
-            target.SendToJail();
-            PlayerManager.BroadcastToAuditChannel(session.Player, $"[Jail] Player {target.Name} was sent to jail by {session.Player.Name}");
-        }
 
-        [CommandHandler("jailbreak", AccessLevel.Admin, CommandHandlerFlag.RequiresWorld,
-            "Releases a player from jail.",
-            "Usage: /jailbreak [playername]\nIf no name is provided, the currently selected player is used.")]
-        public static void HandleJailbreak(Session session, params string[] parameters)
-        {
-            Player target = CommandHandlerHelper.GetPlayerAsCommandTarget(session, string.Join(" ", parameters));
-            if (target == null) return;
-
-            if (!target.IsInJail())
-            {
-                session.Network.EnqueueSend(new GameMessageSystemChat($"Player {target.Name} is not currently in jail.", ChatMessageType.System));
-                return;
-            }
-
-            target.ReleaseFromJail();
-            PlayerManager.BroadcastToAuditChannel(session.Player, $"[Jail] Player {target.Name} was released from jail by {session.Player.Name}");
-        }
 
         // bankaudit {subcommand} {parameters}
         [CommandHandler("bankaudit", AccessLevel.Developer, CommandHandlerFlag.RequiresWorld, 1,
@@ -1925,28 +1898,6 @@ namespace ACE.Server.Command.Handlers
             // TODO: output
         }
 
-        [CommandHandler("ucmcheck", AccessLevel.Admin, CommandHandlerFlag.RequiresWorld, "Initiates a UCM check on the selected player.")]
-        public static void HandleUCMCheck(Session session, params string[] parameters)
-        {
-            Player target = CommandHandlerHelper.GetPlayerAsCommandTarget(session, string.Join(" ", parameters));
-            if (target == null) return;
-
-            if (target.UCMChecker.IsCheckInProgress())
-            {
-                session.Network.EnqueueSend(new GameMessageSystemChat($"{target.Name} is already undergoing a UCM check.", ChatMessageType.System));
-                return;
-            }
-
-            bool started = target.UCMChecker.Start();
-            if (started)
-            {
-                PlayerManager.BroadcastToAuditChannel(session.Player, $"Admin {session.Player.Name} initiated a UCM check on {target.Name}.");
-            }
-            else
-            {
-                session.Network.EnqueueSend(new GameMessageSystemChat($"UCM check on {target.Name} failed to start.", ChatMessageType.System));
-            }
-        }
 
         // gag < char name >
         [CommandHandler("gag", AccessLevel.Sentinel, CommandHandlerFlag.RequiresWorld, 1,
